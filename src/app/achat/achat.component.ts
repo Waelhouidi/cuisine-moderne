81905bab3303a16d228ea1b8244966d27c184b90
Waelhouidi <waelhwidi0@gmail.com>
2024-11-01 20:56:52 +0100

    first compt achat

---
+++ src/app/achat/achat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achat',
+  standalone: true,
+  imports: [],
+  templateUrl: './achat.component.html',
+  styleUrl: './achat.component.css'
+})
+export class AchatComponent {
+
+}
